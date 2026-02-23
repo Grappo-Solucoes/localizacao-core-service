1bbb8dbc9750f83b7b1660b173f9672b61cb245e
Álvaro Zago <43797853+AlvaroAugustoZago@users.noreply.github.com>
2026-02-23 09:20:47 -0300

    fix: Adicionado construtor vazio para os eventos

--- src/main/java/com/busco/localizacao_core_service/domain/events/AlunoPosicaoAtualizadaEvent.java
+++ src/main/java/com/busco/localizacao_core_service/domain/events/AlunoPosicaoAtualizadaEvent.java
@@ -6,4 +6,49 @@ public class AlunoPosicaoAtualizadaEvent {
     public double latitude;
     public double longitude;
     public long timestamp;
+
+    public AlunoPosicaoAtualizadaEvent() {
+    }
+
+
+
+    public String getAlunoId() {
+        return alunoId;
+    }
+
+    public void setAlunoId(String alunoId) {
+        this.alunoId = alunoId;
+    }
+
+    public String getViagemId() {
+        return viagemId;
+    }
+
+    public void setViagemId(String viagemId) {
+        this.viagemId = viagemId;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
